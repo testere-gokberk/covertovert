e9a84f41c1fc6ab046e5479b68348cd2fff3552e
testere-gokberk <gokberk.halacli@metu.edu.tr>
2024-11-04 05:28:22 -0500

    sender receiver done

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,14 @@
 import scapy
 
 # Implement your ICMP receiver here
+
+from scapy.layers.inet import IP, ICMP
+from scapy.sendrecv import sniff
+
+def handle_packet(packet):
+
+    if packet.haslayer(ICMP) and packet[ICMP].type == 8 and packet[IP].ttl == 1:
+            packet.show()
+
+
+sniff(filter="icmp", prn=handle_packet)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,17 @@
 import scapy
 
 # Implement your ICMP sender here
+
+from scapy.layers.inet import IP, ICMP
+from scapy.sendrecv import send
+
+def send_icmp_request(target_ip):
+
+    packet = IP(dst=target_ip, ttl=1) / ICMP()
+    
+    send(packet)
+
+
+target_ip = "172.18.0.2"
+
+send_icmp_request(target_ip)
