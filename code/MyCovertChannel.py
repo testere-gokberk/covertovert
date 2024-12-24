8da35116f0d960458e9d2f7a16047a3a81f49d2d
unknown <halacligokberk@gmail.com>
2024-12-24 23:17:30 +0300

    final ?

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -5,6 +5,7 @@ from scapy.all import get_if_hwaddr, conf
 from scapy.layers.l2 import LLC
 from scapy.layers.l2 import SNAP
 import time
+import random
 
 class MyCovertChannel(CovertChannelBase):
     """
@@ -16,248 +17,123 @@ class MyCovertChannel(CovertChannelBase):
         - You can edit __init__.
         """
         super().__init__()
-        
-    def get_mac(self, ip):
-        
-        arp_req = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip)
-        
-        answered, unanswered = srp(arp_req, timeout=5, verbose=False)
-
-        for _, received in answered:
-            return received.hwsrc  
-
-    def encode(self, message):
-        
-        """
-        - For encoding we map bit all patterns with length 4 to each other and keep the mappings in a dict.
-        """
-        
-        
-        encode_dict = {
-            '0000':'1100', 
-            '0001':'0001', 
-            '0010':'0111', 
-            '0011':'0100', 
-            '0100':'0110',
-            '0101':'1111', 
-            '0110':'1010', 
-            '0111':'1110', 
-            '1000':'1011', 
-            '1001':'1101', 
-            '1010':'0000', 
-            '1011':'1000', 
-            '1100':'0011',
-            '1101':'0010',
-            '1110':'0101',
-            '1111':'1001'
-        }
-
-        message_encoded = []
-        
-        for i in range(0, len(message), 4):
-            message_encoded.append(encode_dict[message[i: i+4]])
-
-        return "".join(message_encoded)
-
-    def decode(self, message):
            
-        """
-        - For decoding we map bit all patterns with length 4 to to their original values to reverse the effect of decoding.
-        """   
-           
-        decode_dict = {
-            '1100':'0000', 
-            '0001':'0001', 
-            '0111':'0010', 
-            '0100':'0011', 
-            '0110':'0100',
-            '1111':'0101', 
-            '1010':'0110', 
-            '1110':'0111', 
-            '1011':'1000', 
-            '1101':'1001', 
-            '0000':'1010', 
-            '1000':'1011', 
-            '0011':'1100',
-            '0010':'1101',
-            '0101':'1110',
-            '1001':'1111'
-        }
-
-        message_decoded = []
-        
-        for i in range(0, len(message), 4):
-            message_decoded.append(decode_dict[message[i: i+4]])
-
-        return "".join(message_decoded)
-        
-    def send(self, log_file_name, parameter1:str, parameter2):
+    def send(self, log_file_name, parameter1, parameter2):
         """
         - In this covert channel implementation, I am using DSAP field in the LLC header to send the secret message.
-        - DSAP(Destination Service Access Point) field determines the network layer address of the device that we are sending the message to.
-        - DSAP is a 8 bit field where its most significant 7 bit stores network layer address of the destination device. Remaining least significant bit determines if the message is broadcasted or not. 
-        - If it is 1, message is broadcasted, so we use broadcasting MAC address in the Ether header.
-        - If it is 0, message is directed to single device so we directly use the MAC address of target device in the Ether header. To find the MAC adress of the target device we broadcast an ARP request once at the beginning.
-        - Parameter1 is for the IP adress of the target device.
+        - We process the message two bits by two bits and every possible two bit pattern corresponds to a range of dsap values:
+        - 00 -> [0, 63]
+        - 01 -> [64, 127]
+        - 10 -> [128, 191]
+        - 11 -> [192, 255]
         """
-        
-        mac_addr = self.get_mac(parameter1)
-        
-        #print("MAC OF TARGET FOUND ", mac_addr)
-        
+    
         binary_message = self.generate_random_binary_message_with_logging(log_file_name,min_length=16,max_length=16)
-        
-        #print("BINARY MESSAGE")
-        
-        #for i, bit in enumerate(binary_message):
-            
-        #    print( binary_message[i: i+4], end=" ")
-                
-        binary_message = self.encode(binary_message)
-        
-        #print("ENCODED BINARY MESSAGE : ", binary_message)
 
         own_mac = self.get_current_mac()
-        #print("MAC OF THIS DEVICE ", own_mac)
         
         start = None
         end = None
         
-        for i,bit in enumerate(binary_message):
+        for i in range(0, len(binary_message), 2):
+
+            dsap = None
+            #print(binary_message[i : i+2])
             
-            #print( type(bit))
-            
-            if bit == "1":
+            if binary_message[i : i+2] == "00":
+                dsap = random.randint(0, 63)
+
+            elif binary_message[i : i+2] == "01":
+                dsap = random.randint(64, 127)
+
+            elif binary_message[i : i+2] == "10":
+                dsap = random.randint(128, 191)
                 
-                base_dsap = 0xc0  
-                encoded_dsap = base_dsap | 0x01
+            elif binary_message[i : i+2] == "11":
+                dsap = random.randint(192, 255)                
 
-                #print("ENCODED DSAP with 1", hex(encoded_dsap))
+            packet = Ether(dst="ff:ff:ff:ff:ff:ff", src=own_mac) / LLC(dsap=(dsap))
 
-                #packet = LLC(dsap=encoded_dsap) / IP(dst="172.18.0.3") 
+            if(i==0):
+                start = time.time()
+
+            super().send(packet)
                 
-                #02:42:ac:12:00:03 ff:ff:ff:ff:ff:ff
-                # src=own_mac
-                
-                packet = Ether(dst="ff:ff:ff:ff:ff:ff", src=own_mac) / LLC(dsap=encoded_dsap) 
-
-                #print("PACKET SENT ", packet.show(dump=True) , "\n PACKET BIT ", bit)
-
-                if(i==0):
-                    start = time.time()
-                    
-                super().send(packet)
-                
-                if(i==len(binary_message)-1):
-                    end = time.time()
-
-            else:
-                
-                base_dsap = 0xc0  
-                encoded_dsap = base_dsap & 0xFE
-
-                #print("ENCODED DSAP with 0", hex(encoded_dsap))
-                packet = Ether(dst=mac_addr ,src=own_mac) / LLC(dsap=(encoded_dsap)) 
-
-                #print("PACKET SENT ", packet.show() , "\n PACKET BIT ", bit)
-                
-                if(i==0):
-                    start = time.time()
-
-                super().send(packet)
-                
-                if(i==len(binary_message)-1):
-                    end = time.time()
-
-        print("ELAPSED TIME ", end-start)
-        print("CHANEL CAPACITY ", 128/(end-start))
+        end = time.time()
+        
+        #print("ELAPSED TIME ", end-start)
+        #print("CHANNEL CAPACITY ", 128/(end-start))
 
     def get_current_mac(self):
-        iface = conf.iface
-        mac_address = get_if_hwaddr(iface)
+
+        """
+        - An utility function to get the MAC adress of the device that we run this script.
+        - Since it is necessary, we use this function to provide the MAC adress of the sending device in the Ether header.
+        """
+
+        mac_address = get_if_hwaddr(conf.iface)
         return mac_address
         
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
-        - To receive the covert channel message, we sniff the packets if the packet has an LLC header we extract the dsap field and apply a mask to get the least significant bit. That bit is added to binary_message string.
-        - If the last 9 bits converts to ".", we terminate the sniffing and log the message to a file.
+        - To receive the covert channel message, we sniff the packets if the packet has an LLC header.
+        - We extract two bits from each packet depending on the which range of dsap values it falls into:
+        - [0, 63] -> 00 
+        - [64, 127] -> 01 
+        - [128, 191] -> 10
+        - [192, 255] -> 11
+        
         """
-        #self.log_message("", log_file_name)
-
-        #mac_addr = self.get_current_mac()
-        #print("MAC ADDRESS OF THIS CONTAIENER ", mac_addr)
 
         self.binary_message = ""
 
         def packet_callback(packet):
             
-            #print("CATCHED THE PACKET ", packet.show(dump=True))
-            
-            counter = 0
-            
-            while True:
-                layer = packet.getlayer(counter)
-                if layer is None:
-                    break
-
-                print(layer)
-                counter += 1
-
             if packet.haslayer(LLC):
-                #print("HAS LLC")
-                
+
                 dsap_value = packet[LLC].dsap
-
-                secret_bit = (dsap_value & 1) 
-
-                #print("SECRET BIT ", secret_bit)
+                secret_bit = None
                 
-                self.binary_message += str(secret_bit)
+                if 0 <= dsap_value and dsap_value <= 63:
+                    secret_bit = "00"
+                elif 64 <= dsap_value and dsap_value <= 127:
+                    secret_bit = "01"
+                elif 128 <= dsap_value and dsap_value <= 191:
+                    secret_bit = "10"
+                elif 192 <= dsap_value and dsap_value <= 255:
+                    secret_bit = "11"
+                    
+                self.binary_message += secret_bit
 
-                if len(self.binary_message) % 8 == 0:  
-                    #char = self.convert_eight_bits_to_character(self.binary_message[-8:])
-                    if self.binary_message[-8:] == "01110101":
-                        #print("TERMINATION CHAR FOUND")
-                        return True
+                if self.convert_eight_bits_to_character(self.binary_message[-8:]) == ".":
+                    return True
 
             return False
 
         def check_stop(packet):
-
-            #print("CATCHED THE PACKET ", packet.show(dump=True))
-
-            if packet.haslayer(LLC):
-
-                #print("HAS LLC")
-                
-                dsap_value = packet[LLC].dsap
-
-                secret_bit = (dsap_value & 1) 
-
-                #print("SECRET BIT ", secret_bit)
-
-                if len(self.binary_message) % 8 == 0:  
-                    #char = self.convert_eight_bits_to_character(self.binary_message[-8:])
-                    if self.binary_message[-8:] == "01110101":
-                        #print("TERMINATION CHAR FOUND")
-                        return True
-                    
+            
+            if packet.haslayer(LLC):             
+                if self.convert_eight_bits_to_character(self.binary_message[-8:]) == ".":
+                    return True
+            
             return False
 
         sniff(iface="eth0", prn=packet_callback, stop_filter=check_stop)
 
-        self.binary_message = self.decode(self.binary_message)
+        char_msg = []
+        
+        for i in range(0, len(self.binary_message), 8):
+            char_msg.append(self.convert_eight_bits_to_character(self.binary_message[i:i+8]))
 
-
-
-        decoded_message = "".join(
-            self.convert_eight_bits_to_character(self.binary_message[i:i + 8])
-            for i in range(0, len(self.binary_message), 8)
-        )
+        decoded_message = "".join(char_msg)
 
         self.log_message(decoded_message, log_file_name)
 
         return decoded_message
+
+
+
+
 
 
 
--- code/README.md
+++ code/README.md
@@ -3,20 +3,21 @@
 **Covert Channel**
 
 We are sending our covert channel message using the DSAP field of LLC header. 
-The secret message is carried in the least significant bit of DSAP field which controls whether the message is sent to a single device or a group of devices. Depending on that value we either use a broadcasting MAC adress or the individual MAC adress of the target device. 
+To convey our message we ate using all possible values of the 8 bits of the DSAP field.
+We are sending two bits from the secret message per packet.
 
 **Encoding Method**
 
-We apply a simple encoding mechanism where we map every possible bit pattern with length of 4 to each other. 
-For decoding, we apply the reverse of mappings used in encoding to recover the original message.
+To encode our message we are mapping every possible 2 bit combinations into different range of DSAP values.
+    
+    - Bit Values -> DSAP Ranges
+    - 00 -> [0, 63]
+    - 01 -> [64, 127]
+    - 10 -> [128, 191]
+    - 11 -> [192, 255]
 
-    For example:
-
-        Encode: 0000 -> 1000
-        Decode: 1000 -> 0000
-
-We store the mappings in a dictionary. We encode the whole message before transmitting and decode it after receiving it completely.
+For decoding, we apply the reverse of this and extract the bit values which DSAP value of the packet corresponds to.
 
 **Covert Channel Performance**
 
-Our covert channel capacity is ~12.11 bps
+Our covert channel capacity is ~23 bps
